c21aca37ec8cdf457eb424aa016982e73295f955
Shawn T. Amundson <amundson@bitwise.io>
2020-03-09 13:32:16 -0500

    WIP

--- libsplinter/src/transport/mod.rs
+++ libsplinter/src/transport/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "http-transport")]
+pub mod http;
 pub mod inproc;
 pub mod multi;
 pub mod raw;
